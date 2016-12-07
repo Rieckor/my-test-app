495f7be6b253fcda721b80ccb3930d3de6d317a6
rk890316 <304825094@qq.com>
2016-12-07 17:43:33 +0800

    实现post传输

--- src/app/register/step/second-step/second-step.component.ts
+++ src/app/register/step/second-step/second-step.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RegisterService } from '../../register-http.service';
 
 @Component({
   selector: 'app-second-step',
   templateUrl: './second-step.component.html',
-  styleUrls: ['./second-step.component.css']
+  styleUrls: ['./second-step.component.css'],
+  providers: [RegisterService]
 })
-export class SecondStepComponent implements OnInit {
+export class SecondStepComponent  {
+  cnt =  0;
+  constructor(
+    private regService: RegisterService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  characterCount(event): number {
+    return this.cnt = event.value.length;
   }
 
+  subReg(data) {
+    this.regService.postData(data)
+    .subscribe (
+      any => { console.log(any); },
+      error => {}
+    );
+  }
 
 }
--- src/app/register/step/second-step/uploadQR.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, OnChanges, Input, SimpleChanges } from '@angular/core';
-
-@Directive({ selector: '[uploadQR]' })
-export class UploadQRDirective {
-     @Input() avatar: String;
-    ngOnChanges(changes: SimpleChanges) {
-
-  }
-}
