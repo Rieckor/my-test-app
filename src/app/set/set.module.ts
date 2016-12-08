17b23603f349295b1af4303617a32b2d79d803e1
rk890316 <304825094@qq.com>
2016-12-08 17:56:08 +0800

    add set

--- src/app/set/set.module.ts
+++ src/app/set/set.module.ts
@@ -3,8 +3,6 @@ import { FormsModule }    from '@angular/forms';
 import { CommonModule }   from '@angular/common';
 
 import { SetComponent } from './set.component';
-import { UploadAvatarComponent } from '../share/upload-avatar.component';
-import { FileSelectDirective } from 'ng2-file-upload';
 import { SetRouteModule } from './set-route.module';
 import { BaseComponent } from './base/base.component';
 import { PrecisionComponent } from './Precision/Precision.component';
@@ -20,9 +18,7 @@ import { IndexComponent } from './Index/Index.component';
     SetComponent,
     BaseComponent,
     PrecisionComponent,
-    IndexComponent,
-    UploadAvatarComponent,
-    FileSelectDirective
+    IndexComponent
   ],
   exports: [
     BaseComponent,
