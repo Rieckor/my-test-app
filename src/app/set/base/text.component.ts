547fa0de0ea32b232b83ffd1e6f0bdd8a7bcf3ff
rk890316 <304825094@qq.com>
2016-12-14 17:47:18 +0800

    text

--- src/app/set/base/text.component.ts
+++ src/app/set/base/text.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../share/user';
+import { SharedData } from '../../core/share-data.service';
 
 @Component({
   template: `
   <div class="weui-cell" style="padding: 1rem;" >
     <div class="weui-cell__bd" style="display: flex;border-bottom: 1px solid #3FCA3F;">
-        <input #text class="weui-input" (keyup)="characterCount(text)" placeholder="请输入文本" maxlength="50"  type="text">
+        <input #text class="weui-input" 
+        (keyup)="characterCount(text)" 
+        placeholder="请输入文本" 
+        maxlength="50" 
+        value="{{user?.self_instruction}}"  
+        type="text">
         <div class="weui-input-counter" style="color: #A5A5A5;">
             <span>{{cnt}}</span>/50
         </div>
@@ -16,9 +23,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextComponent implements OnInit {
   cnt =  0;
-  constructor() { }
+  user: User;
+  constructor(
+     private sharedata: SharedData
+  ) { }
 
   ngOnInit() {
+    this.sharedata.getData().subscribe(
+      user => {
+        if (user != null) {
+           this.user = user;
+        }
+      }
+      );
   }
   characterCount(event): number {
     return this.cnt = event.value.length;
