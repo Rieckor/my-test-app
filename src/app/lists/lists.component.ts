cff66cede544dcee2ab997b2f87fede40ef2c64f
rk8903161 <304825094@qq.com>
2017-01-04 23:04:18 +0800

    update menu style

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,25 +1,26 @@
 // Add the RxJS Observable operators.
 import '../rxjs-operators';
 
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MdIconRegistry} from '@angular/material';
+import {MdMenuTrigger} from '@angular/material';
 
 @Component({
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css'],
   providers: [MdIconRegistry]
 })
+ 
 export class ListsComponent {
+  @ViewChild(MdMenuTrigger) trigger: MdMenuTrigger;
   navItems = [
     {name: '人脉列表', route: 'index'},
     {name: '群落列表', route: 'group'},
-    {name: '精准列表', route: 'precision'},
-    {name: '注册', route: '/register/step1'},
-    {name: '设置', route: '/set/index'},
-    {name: 'Gestures', route: '/notice/1'},
-    {name: 'Grid List', route: '/notice/2'}
+    {name: '精准列表', route: 'precision'}
   ];
-
+  someMethod() {
+    this.trigger.openMenu();
+  }
 }
 
 
--- src/app/share/shared.module.ts
+++ src/app/share/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule }            from '@angular/core';
 import { CommonModule }        from '@angular/common';
 import { FormsModule }         from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import {OVERLAY_PROVIDERS} from "@angular/material";
 import { UploadAvatarComponent } from './upload-avatar.component';
 import { FileSelectDirective } from 'ng2-file-upload';
 
@@ -11,6 +12,7 @@ import { MainPipe } from '../pipe/main-pipe.module';
 @NgModule({
   imports:      [ CommonModule, MainPipe, MaterialModule ],
   declarations: [ UploadAvatarComponent, FileSelectDirective ],
+  providers: [OVERLAY_PROVIDERS],
   exports:      [CommonModule, FormsModule, MaterialModule, MainPipe, FileSelectDirective, UploadAvatarComponent ]
 })
 export class SharedModule { }
